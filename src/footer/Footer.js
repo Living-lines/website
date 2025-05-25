99d9a6e2d0b7fadc41fef8f1fac2aa7692030aef
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-05-25 22:04:28 +0530

    subscribers global api is added

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -28,7 +28,7 @@ const Footer = () => {
     }
 
     try {
-      const res = await fetch('http://localhost:3000/api/subscribers', {
+      const res = await fetch('http://backend-tawny-one-62.vercel.app/api/subscribers', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, mobile }),
